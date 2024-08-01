9caafeddf25376ecb8b85d5c1e4b8ed6a13efef3
samaniheo <tiosanjaya635@gmail.com>
2024-08-01 14:56:03 +0700

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,5 @@
+apt-get update; apt install -y screen; apt install -y sudo
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt install -y g++-11
+echo done 50%
